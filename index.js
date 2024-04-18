3e9619d8f49cfd3e92bf1d25f740152954ebaae8
Sagar Jaswal <sagarjaswal9102001@gmail.com>
2024-04-18 15:05:14 +0530

    Setting up mongoDB connection

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-const express = require("express")
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.listen(8080, (req, res) => {
+const port = process.env.PORT || 8080;
+
+mongoose.connect('mongodb://127.0.0.1/monterassignment', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+    console.log('Connected to MongoDB');
+});
+
+app.use(express.json());
+
+app.listen(port, (req, res) => {
     console.log("Listening to requests on port 8080");
 });
